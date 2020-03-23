88e6068a04d63d42b04d16af5899cd75d8089dfa
nazuna <nazuna11develop@gmail.com>
2020-03-23 18:09:54 +0900

    [update] persist witelist

--- ws-app/src/index.js
+++ ws-app/src/index.js
@@ -15,6 +15,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'root',
   storage,
+  whitelist: ['message']
 }
 
 const persistedReducer = persistReducer(persistConfig, reducer);
